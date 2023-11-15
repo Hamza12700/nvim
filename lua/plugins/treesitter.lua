029ed79f8a6b7ced73bb3781a208b231f61cfd21
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-11-15 10:22:54 -0700

    added `treesitter.lua` to auto install syntax highlighting for some languages

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "css",
+      "scss",
+      "astro",
+      "svelte",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "typescript",
+      "vim",
+      "yaml",
+      "toml",
+    },
+  },
+}
