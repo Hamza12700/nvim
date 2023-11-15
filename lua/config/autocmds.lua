7974f244b32d120e952eda25411de6eb573f1780
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-11-15 06:55:05 -0700

    turn on paste mode when leaving insert-mode

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,9 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Turn off paste mode when leaving insert-mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
