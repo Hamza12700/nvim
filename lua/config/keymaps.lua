e7493d27c2505c252222e3ff64d45256688c532b
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-11-20 04:36:50 -0700

    remove keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,25 +21,9 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
--- New tab
-keymap.set("n", "te", "tabedit", opts)
-keymap.set("n", "tab", "tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
-
 -- Split windows
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
-
--- Move windows
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
--- Resize windows
-keymap.set("n", "<C-w>left", "<C-w><")
-keymap.set("n", "<C-w>right", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostic
 keymap.set("n", "<C-j>", function()
