2a0eedd9ef4c6c071d17be3c652f1bd1fe76256a
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-28 19:07:37 +0400

    added(keybinds): To put the cursor in the center when page Down/Up

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
