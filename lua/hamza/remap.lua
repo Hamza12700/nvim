5c292fcc7f951e579c11e6245b0ddcc41a754d56
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-28 22:53:28 +0400

    added(keybinds): For pasting it don't replace the highlighted text and same for yanking

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -13,5 +13,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
