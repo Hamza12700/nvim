accc8efc704190a50a9860f6c279a118e693385a
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-28 19:08:17 +0400

    added(keybinds): To keep the cursor in the centor when searching the result

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -9,3 +9,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
