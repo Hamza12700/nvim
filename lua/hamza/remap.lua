ba41d07efcae58702df37e9923e1aedc461af87e
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-28 19:06:59 +0400

    added(keybinds): To move highlighted text up/down

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -3,4 +3,6 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
