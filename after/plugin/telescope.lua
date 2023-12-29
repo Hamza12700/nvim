87d180486c2a2825e7ea0b7e0889a60a6f62ab0c
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-29 17:16:33 +0400

    fixed(typo): In `telescope.lua`

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,30 @@
-local buitin = require("telescope.builtin")
+local builtin = require("telescope.builtin")
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+			},
+		},
+	},
+})
 
-vim.keymap.set('n', '<leader>pf', buitin.find_files, {})
-vim.keymap.set('n', '<leader>?', buitin.oldfiles, {})
-vim.keymap.set('n', '<leader>b', buitin.buffers, {})
-vim.keymap.set('n', '<C-p>', buitin.git_files, {})
-vim.keymap.set('n', '<leader>ps', buitin.live_grep, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 
+vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+
 vim.keymap.set('n', '<leader>/', function ()
-	buitin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 14,
 		previewer = false
 	}))
