30e98ea9ecbcb819a4a8b32863b7d68568115821
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-29 17:19:51 +0400

    change(`vim.opt`): To `local opt = vim.opt`
    So I don't have to type the full thing again and again

--- lua/hamza/options.lua
+++ lua/hamza/options.lua
@@ -3,42 +3,43 @@ local opt = vim.opt
 opt.scrolloff = 10
 
 -- Set highlight on search
-vim.o.hlsearch = false
+opt.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
-vim.opt.relativenumber = true
+opt.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = "a"
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+opt.mouse = "a"
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
 -- Enable break indent
-vim.o.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.o.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = "yes"
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+opt.updatetime = 250
+opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+opt.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-vim.o.softtabstop = 2
+opt.termguicolors = true
+opt.softtabstop = 2
