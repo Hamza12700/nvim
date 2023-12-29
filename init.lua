a1cefb42694b4afadedf079cf2c0880a7850751b
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-29 16:36:09 +0400

    moved(`vim.opt`): In `/lua/hamza/options.lua` file

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("hamza.remap")
+require("hamza.options")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -52,7 +53,7 @@ require("lazy").setup({
 
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+			"j-hui/fidget.nvim",
 
 			-- Additional lua configuration, makes nvim stuff amazing!
 			"folke/neodev.nvim",
@@ -76,11 +77,8 @@ require("lazy").setup({
 		},
 	},
 
-	{
-		-- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {},
-	},
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	"lewis6991/gitsigns.nvim",
 
 	{
 		"rebelot/kanagawa.nvim",
@@ -125,51 +123,6 @@ require("lazy").setup({
 		build = ":TSUpdate",
 	},
 }, {})
-
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-vim.opt.relativenumber = true
-
--- Enable mouse mode
-vim.o.mouse = "a"
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
 
---
+++ lua/hamza/options.lua
@@ -0,0 +1,44 @@
+local opt = vim.opt
+
+opt.scrolloff = 10
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+vim.o.mouse = "a"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect"
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+vim.o.softtabstop = 2
