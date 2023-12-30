b12bc0baf077b1536120f007b45c14d3786fff81
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-30 21:09:05 +0400

    commented out: `vim.api.nvim_set_hl(0, "NormalFloat", { bg = "nono" })

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,7 +3,7 @@ function colorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 colorMyPencils()
