c2f0cda8bdf7705b003bdc12341b716ad2b12d2f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-22 23:15:42 +0400

    added: keybind in insert to show signature help: `<C-k>`

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -26,3 +26,4 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>f", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
