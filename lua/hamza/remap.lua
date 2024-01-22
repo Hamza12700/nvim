9c976c179799a7236fe4805a622691958c59c3c0
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-22 22:59:40 +0400

    added: keymap to `chmod +x` the open file

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -18,6 +18,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
