3eb919dc06c687a00573693697c273a1b4614ad9
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-25 21:52:35 +0400

    feat: keybind for quiting: `<leader>q`

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -3,6 +3,7 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
