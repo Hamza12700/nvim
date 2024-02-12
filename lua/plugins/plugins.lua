ffb58ac5f60b8cee6f240407e79838f3e03065ea
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-12 21:17:18 +0400

    feat: `nvim-spectre` for global find and replace with `rg`

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,10 +1,21 @@
 return {
-
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		config = function ()
+		config = function()
 			require("nvim-autopairs").setup()
+		end
+	},
+
+	{
+		"nvim-pack/nvim-spectre",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local spectre = require("spectre")
+			vim.keymap.set("n", "<leader>S", ":Spectre<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>rw", function()
+				spectre.open_visual({ select_word = true })
+			end)
 		end
 	},
 
