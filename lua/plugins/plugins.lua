7a9704c4cfacc04cdbedfe884477fd6e43657584
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-13 22:44:47 +0400

    feat: using latest version of luasnip

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -69,11 +69,16 @@ return {
 	},
 
 	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp"
+	},
+
+	{
 		-- Autocompletion
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			-- Snippet Engine & its associated nvim-cmp source
-			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 
 			-- Adds LSP completion capabilities
