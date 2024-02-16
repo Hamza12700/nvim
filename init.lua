09d18b59ac23ead05fee7c4a0ef822a89eeb7525
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-16 21:12:16 +0400

    feat: `nvim-cmp` keybind to abort auto-complete

--- init.lua
+++ init.lua
@@ -166,6 +166,7 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-e>"] = cmp.mapping.abort(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
