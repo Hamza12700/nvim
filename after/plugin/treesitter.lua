318493b8da85212925115b33c091768cb4dfe878
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-19 21:22:37 +0400

    change: nvim-treesitter text objects keybinds

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -52,20 +52,26 @@ vim.defer_fn(function()
 			move = {
 				enable = true,
 				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next = {
+					["]d"] = "@conditional.outer",
+				},
+				goto_previous = {
+					["[d"] = "@conditional.outer"
+				},
 				goto_next_start = {
-					["]m"] = "@function.outer",
+					["]f"] = "@function.outer",
 					["]]"] = "@class.outer",
 				},
 				goto_next_end = {
-					["]M"] = "@function.outer",
+					["]F"] = "@function.outer",
 					["]["] = "@class.outer",
 				},
 				goto_previous_start = {
-					["[m"] = "@function.outer",
+					["[f"] = "@function.outer",
 					["[["] = "@class.outer",
 				},
 				goto_previous_end = {
-					["[M"] = "@function.outer",
+					["[F"] = "@function.outer",
 					["[]"] = "@class.outer",
 				},
 			},
