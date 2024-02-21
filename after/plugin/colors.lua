1ca301824993028ddfc84296b0a86287c339ce28
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-21 20:37:06 +0400

    refactor: colorscheme function

--- after/plugin/colors.lua
+++
@@ -1,9 +0,0 @@
-function colorMyPencils(color)
-	color = color or "kanagawa"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-colorMyPencils()
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -101,7 +101,8 @@ return {
 		"rebelot/kanagawa.nvim",
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme kanagawa")
+			vim.cmd.colorscheme("kanagawa")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		end,
 	},
 
