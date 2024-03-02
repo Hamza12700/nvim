9d1a3c4479ef4f54f11ce4a47e1de6c0d283e4f5
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-02 14:52:35 +0400

    feat: keybinds for luasnip to move left and right of each of the expansion list

--- init.lua
+++ init.lua
@@ -176,6 +176,16 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
+    ['<C-l>'] = cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      end
+    end, { 'i', 's' }),
+    ['<C-h>'] = cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { 'i', 's' }),
   }),
   sources = {
     { name = "nvim_lsp" },
