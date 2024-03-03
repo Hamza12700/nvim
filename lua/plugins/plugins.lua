b54163e927c5ad12710ad494400a683d8783f893
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-03 19:36:20 +0400

    change: some options for the colorscheme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -108,6 +108,11 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		priority = 1000,
+		build = ":KanagawaCompile",
+		opts = {
+			compile = true,
+			dimInactive = true,
+		},
 		config = function()
 			vim.cmd.colorscheme("kanagawa")
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
