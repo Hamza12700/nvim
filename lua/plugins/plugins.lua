0f9b5634d4acf02e8d61d19325e49edc9316cfd8
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-06 20:11:49 +0400

    configure: lualine disable component and sections separators

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -124,6 +124,10 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
+			options = {
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
 			theme = "auto",
 			sections = {
 				lualine_x = { "filetype" }
