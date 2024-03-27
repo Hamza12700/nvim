404f2161e4949054ee0185cd3c6d032bad3dde7a
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-27 13:05:44 +0400

    feat: run templ generate on every templ file

--- ftplugin/templ.lua
+++ ftplugin/templ.lua
@@ -1,3 +1,11 @@
 vim.keymap.set("n", "<leader>o", function ()
   vim.cmd("!templ fmt %")
 end)
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("Templ Generate", { clear = true }),
+  pattern = "*.templ",
+  callback = function ()
+    vim.cmd(":silent !templ generate -f %")
+  end
+})
