fbb14c62aaeaf4e20576c03c2dcbbf8bf3811cb8
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-04-27 17:57:00 +0000

    remove: kanagawa compile option

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -92,11 +92,6 @@ return {
   {
     "rebelot/kanagawa.nvim",
     priority = 1000,
-    build = ":KanagawaCompile",
-    opts = {
-      compile = true,
-      dimInactive = true,
-    },
     config = function()
       vim.cmd.colorscheme("kanagawa")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
