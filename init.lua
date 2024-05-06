2ad008d55665195a2a18a520c829c66131c35879
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-05-06 16:25:42 +0000

    formatted

--- init.lua
+++ init.lua
@@ -14,7 +14,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.filetype.add({ extension = { templ = "templ" } })
+vim.filetype.add({
+  extension = { templ = "templ" }
+})
 
 require("lazy").setup("plugins")
 
