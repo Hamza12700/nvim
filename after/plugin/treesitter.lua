6a89aa3594254d15fe23b44fdd0ce1c2844cf650
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-06-12 15:44:44 +0000

    treesitter: set auto_install to true

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -23,7 +23,7 @@ vim.defer_fn(function()
 		additional_vim_regex_highlighting = false,
 
 		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-		auto_install = false,
+		auto_install = true,
 
 		highlight = { enable = true },
 		indent = { enable = true },
