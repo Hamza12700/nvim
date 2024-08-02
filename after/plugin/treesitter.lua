ba25e1ea04d0b6bf20186768ac003f062a6f0ff7
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-08-02 12:36:26 +0000

    added: new keybinds to treesitter text-object

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,6 +2,7 @@
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
+	---@diagnostic disable-next-line: missing-fields
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = {
 			"c",
@@ -41,13 +42,12 @@ vim.defer_fn(function()
 				enable = true,
 				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
 					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
+					["ai"] = "@conditional.outer",
 					["af"] = "@function.outer",
+					["al"] = "@loop.outer",
+					["ia"] = "@parameter.inner",
 					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
 					["a="] = "@assignment.inner",
 					["as"] = "@assignment.lhs",
 				},
@@ -55,38 +55,32 @@ vim.defer_fn(function()
 			move = {
 				enable = true,
 				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next = {
-					["]i"] = "@conditional.outer",
-				},
-				goto_previous = {
-					["[i"] = "@conditional.outer"
-				},
 				goto_next_start = {
 					["]f"] = "@function.outer",
-					["]]"] = "@class.outer",
+					["]i"] = "@conditional.outer",
+					["]l"] = "@loop.outer",
 				},
 				goto_next_end = {
-					["]F"] = "@function.outer",
-					["]["] = "@class.outer",
+					["[f"] = "@function.outer",
+					["[i"] = "@conditional.outer"
 				},
 				goto_previous_start = {
-					["[f"] = "@function.outer",
-					["[["] = "@class.outer",
+					["]F"] = "@function.outer",
+					["]I"] = "@conditional.outer",
 				},
 				goto_previous_end = {
 					["[F"] = "@function.outer",
-					["[]"] = "@class.outer",
+					["[I"] = "@conditional.outer",
 				},
 			},
-			-- swap = {
-			-- 	enable = true,
-			-- 	swap_next = {
-			-- 		["<leader>a"] = "@parameter.inner",
-			-- 	},
-			-- 	swap_previous = {
-			-- 		["<leader>A"] = "@parameter.inner",
-			-- 	},
-			-- },
+			lsp_interop = {
+				enable = true,
+				border = 'none',
+				floating_preview_opts = {},
+				peek_definition_code = {
+					["<leader>df"] = "@function.outer",
+				},
+			}
 		},
 	})
 end, 0)
