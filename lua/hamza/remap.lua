2410568b96215f7d5e9b80fc53df61d6e474a8bc
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-12-19 15:56:05 +0000

    feat: keybind to add cargo packages

--- lua/hamza/remap.lua
+++ lua/hamza/remap.lua
@@ -24,11 +24,28 @@ keymap("n", "<leader>g", ":Neogit<CR>")
 keymap("n", "<leader>vd", ":DiffviewOpen<cr>")
 keymap("n", "<leader>cd", ":DiffviewClose<cr>")
 
+local term_id = 0
+keymap("n", "<leader>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 8)
+
+  term_id = vim.bo.channel
+end)
+
+keymap("n", "<leader>CA", function()
+  local pkg = vim.fn.input("Cargo package: ")
+  vim.fn.chansend(term_id, "cargo add " .. pkg .. "\r\n")
+end, { desc = "Add cargo package" })
+
 -- Trouble keybinds
 keymap("n", "<leader>xx", ":Trouble diagnostics toggle focus=true<cr>", { desc = "Diagnostics (Trouble)", })
-keymap("n", "<leader>xb", ":Trouble diagnostics toggle filter.buf=0 focus=true<cr>", { desc = "Buffer Diagnostics (Trouble)", })
+keymap("n", "<leader>xb", ":Trouble diagnostics toggle filter.buf=0 focus=true<cr>",
+  { desc = "Buffer Diagnostics (Trouble)", })
 keymap("n", "<leader>cs", ":Trouble symbols toggle focus=true<cr>", { desc = "Symbols (Trouble)", })
-keymap("n", "<leader>cl", ":Trouble lsp toggle focus=true win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)", })
+keymap("n", "<leader>cl", ":Trouble lsp toggle focus=true win.position=right<cr>",
+  { desc = "LSP Definitions / references / ... (Trouble)", })
 keymap("n", "<leader>xq", ":Trouble qflist toggle focus=true<cr>", { desc = "Quickfix List (Trouble)", })
 keymap("n", "<leader>xl", ":Trouble loclist toggle focus=true<cr>", { desc = "Location List (Trouble)", })
 
