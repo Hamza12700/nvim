30cb4411429b146d895282ca384637f23157b02d
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-05-11 12:39:23 +0000

    remove meason setup_handlers

--- init.lua
+++ init.lua
@@ -84,14 +84,6 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup()
-mason_lspconfig.setup_handlers({
-   function(server_name)
-      require("lspconfig")[server_name].setup({
-         capabilities = capabilities,
-         on_attach = on_attach,
-      })
-   end,
-})
 
 local cmp = require("cmp")
 cmp.setup({
